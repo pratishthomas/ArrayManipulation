38abb0dcf84a37dd130155008e5eb0fb80dd294f
Pratish <pratish.thomas@gmail>
2019-05-13 23:16:05 +1000

    Add project files.

---
+++ DeveloperAssessment.Api/Controllers/ArrayCalcController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArrayManipulation.Api.Utilties;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ArrayManipulation.Api.Controllers
+{
+    [Route("api/arraycalc")]
+    public class ArrayManipulationController : Controller
+    {
+
+        [HttpGet("reverse")]
+        public IActionResult Reverse(int[] productIds)
+        {
+
+            try
+            {
+                return Ok(ArrayUtility.Reverse(productIds));
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error");
+            }
+        }
+
+        [HttpGet("deletepart")]
+        public IActionResult DeletePart(int position, int[] productIds)
+        {
+            try
+            {
+                return Ok(ArrayUtility.DeleteElementAtIndex(position, productIds));
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error");
+            }
+        }
+
+
+    }
+}
---
+++ DeveloperAssessment.Api/Utilties/ArrayUtility.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArrayManipulation.Api.Utilties
+{
+    public class ArrayUtility
+    {
+        /// <summary>
+        /// This function reverses an integer array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int[] Reverse(int[] array)
+        {
+            int startIndex = 0;
+            int temp = 0;
+            int endIndex = array.Length - 1;
+
+            while (startIndex < array.Length / 2)
+            {
+                temp = array[startIndex];
+                array[startIndex] = array[endIndex];
+                array[endIndex] = temp;
+                startIndex++;
+                endIndex--;
+            }
+
+            return array;
+        }
+
+        /// <summary>
+        /// This function removes the element of an array at a specified index
+        /// </summary>
+        /// <returns></returns>
+        public static int[] DeleteElementAtIndex(int index, int[] array)
+        {
+            int[] resizedArray = new int[array.Length - 1];
+
+            for (int i = index; i < array.Length; i++)
+            {
+                array[i - 1] = array[i];
+            }
+
+            for (int i = 0; i < resizedArray.Length; i++)
+            {
+                resizedArray[i] = array[i];
+            }
+
+            return resizedArray;            
+        }
+
+
+    }
+}
